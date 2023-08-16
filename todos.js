dc30e58edcd9bf5efa2b5da3e917be952f2d6e24
HyoSung Bidol-Lee <hyosung11@me.com>
2023-08-16 09:56:24 -0400

    Sort todo lists by title

--- todos.js
+++ todos.js
@@ -14,8 +14,27 @@ app.set("view engine", "pug");
 app.use(morgan("common"));
 app.use(express.static("public"));
 
+// Return the list of todo lists sorted by completion status and title.
+const sortTodoLists = lists => {
+  return lists.slice().sort((todoListA, todoListB) => {
+    let titleA = todoListA.title;
+    let titleB = todoListB.title;
+
+    if (titleA < titleB) {
+      return -1;
+    } else if (titleA > titleB) {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+};
+
+// Render the list of todo lists
 app.get("/", (req, res) => {
-  res.render("lists", { todoLists });
+  res.render("lists", {
+    todoLists: sortTodoLists(todoLists),
+  });
 });
 
 // Listener
