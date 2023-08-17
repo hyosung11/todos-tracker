e413c836e5d083e33e39b3fa889630223247e77b
HyoSung Bidol-Lee <hyosung11@me.com>
2023-08-17 11:07:57 -0400

    Modularize the sorting functions

--- todos.js
+++ todos.js
@@ -5,6 +5,7 @@ const flash = require("express-flash");
 const session = require("express-session");
 const { body, validationResult } = require("express-validator");
 const TodoList = require("./lib/todolist");
+const { sortTodoLists, sortTodos} = require("./lib/sort");
 
 const app = express();
 const host = "localhost";
@@ -39,38 +40,6 @@ app.use((req, res, next) => {
 /* Find a todo list with the indicated ID. Returns `undefined` if not found. Note that `todoListId` must be numeric. */
 const loadTodoList = todoListId => {
   return todoLists.find(todoList => todoList.id === todoListId);
-};
-
-// Compare object titles alphabetically (case-insensitive)
-const compareByTitle = (itemA, itemB) => {
-  let titleA = itemA.title.toLowerCase();
-  let titleB = itemB.title.toLowerCase();
-
-  if (titleA < titleB) {
-    return -1;
-  } else if (titleA > titleB) {
-    return 1;
-  } else {
-    return 0;
-  }
-};
-
-// Return the list of todo lists sorted by completion status and title.
-const sortTodoLists = lists => {
-  let undone = lists.filter(todoList => !todoList.isDone());
-  let done = lists.filter(todoList => todoList.isDone());
-  undone.sort(compareByTitle);
-  done.sort(compareByTitle);
-  return [].concat(undone, done);
-};
-
-// Return the list of todos in the todo list sorted by completion status and title.
-const sortTodos = todoList => {
-  let undone = todoList.todos.filter(todo => !todo.isDone());
-  let done = todoList.todos.filter(todo => todo.isDone());
-  undone.sort(compareByTitle);
-  done.sort(compareByTitle);
-  return [].concat(undone, done);
 };
 
 // Redirect start page
