d4d922d8b52eafd4f0283738746fd6632ad27e0a
HyoSung Bidol-Lee <hyosung11@me.com>
2023-08-18 13:29:19 -0400

    Display the edit todo list page

--- todos.js
+++ todos.js
@@ -223,7 +223,7 @@ app.post("/lists/:todoListId/todos",
 // Render edit todo list form
 app.get("/lists/:todoListId/edit", (req, res, next) => {
   let todoListId = req.params.todoListId;
-  let todoList = loadTodoList(+todoListId, req.session.todoLists);
+  let todoList = res.locals.store.loadTodoList(+todoListId);
   if (!todoList) {
     next(new Error("Not found."));
   } else {
