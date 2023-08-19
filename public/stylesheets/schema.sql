711d5bfeac3421ca6fc6db7782f525f0aff85801
HyoSung Bidol-Lee <hyosung11@me.com>
2023-08-19 12:21:15 -0400

    Create schema for database

---
+++ public/stylesheets/schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE todolists (
+  id SERIAL PRIMARY KEY,
+  title TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE todos (
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  done BOOLEAN NOT NULL DEFAULT false,
+  todolist_id INTEGER NOT NULL REFERENCES todolists (id) ON DELETE CASCADE
+);
