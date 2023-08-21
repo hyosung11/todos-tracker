08bde3b015242cdd295608e0a707ed0552d4bd34
HyoSung Bidol-Lee <hyosung11@me.com>
2023-08-21 14:38:45 -0400

    Load a single todo list

--- todos.js
+++ todos.js
@@ -127,20 +127,23 @@ app.post("/lists",
 );
 
 // Render individual todo list and its todos
-app.get("/lists/:todoListId", (req, res, next) => {
-  let todoListId = req.params.todoListId;
-  let todoList = res.locals.store.loadTodoList(+todoListId);
+app.get("/lists/:todoListId", async (req, res, next) => {
+  try {
+    let todoListId = req.params.todoListId;
+    let todoList = await res.locals.store.loadTodoList(+todoListId);
+    if (todoList === undefined) {
+      next(new Error("Not found."));
+    } else {
+      // todoList.todos = res.locals.store.sortedTodos(todoList);/
 
-  if (todoList === undefined) {
-    next(new Error("Not found."));
-  } else {
-    todoList.todos = res.locals.store.sortedTodos(todoList);
-
-    res.render("list", {
-      todoList,
-      isDoneTodoList: res.locals.store.isDoneTodoList(todoList),
-      hasUndoneTodos: res.locals.store.hasUndoneTodos(todoList),
-    });
+      res.render("list", {
+        todoList,
+        isDoneTodoList: res.locals.store.isDoneTodoList(todoList),
+        hasUndoneTodos: res.locals.store.hasUndoneTodos(todoList),
+      });
+    }
+  } catch(error) {
+    next(error);
   }
 });
 
