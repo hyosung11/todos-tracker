7d1f33f602fd12fe0a24e77ff325def38bba5903
HyoSung Bidol-Lee <hyosung11@me.com>
2023-08-23 15:11:15 -0400

    Create users table

--- schema.sql
+++ schema.sql
@@ -9,3 +9,8 @@ CREATE TABLE todos (
   done BOOLEAN NOT NULL DEFAULT false,
   todolist_id INTEGER NOT NULL REFERENCES todolists (id) ON DELETE CASCADE
 );
+
+CREATE TABLE users (
+  username TEXT PRIMARY KEY,
+  password TEXT NOT NULL
+);
